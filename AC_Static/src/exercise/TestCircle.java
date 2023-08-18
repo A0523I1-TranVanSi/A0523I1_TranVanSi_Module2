73f316c460b2b46900ba9aa72797af64e1459657
A0523I1-TranVanSi <sitran.120503@gmail.com>
2023-08-18 20:39:12 +0700

    circle

---
+++ AC_Static/src/exercise/TestCircle.java
@@ -0,0 +1,9 @@
+package exercise;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle C1 = new Circle(2.5);
+        System.out.println("Ban kinh : " + C1.getRadius());
+        System.out.println("Dien tich : "+C1.getArea());
+    }
+}
