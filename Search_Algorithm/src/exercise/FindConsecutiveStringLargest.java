b291d322ec4a204c9dfb477b01c7b56cac112b27
A0523I1-TranVanSi <sitran.120503@gmail.com>
2023-09-27 18:26:10 +0700

    bt

---
+++ Search_Algorithm/src/exercise/FindConsecutiveStringLargest.java
@@ -0,0 +1,30 @@
+package exercise;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class FindConsecutiveStringLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        System.out.print("Enter string : ");
+        String str  = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (list.size() > 1 && str.charAt(i) <= list.getLast() && list.contains(str.charAt(i))){
+                list.clear();
+            }
+            list.add(str.charAt(i));
+            if (list.size() > max.size()){
+                max.clear();
+                max.addAll(list);
+            }
+        }
+        for (Character ch : max ){
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+}
